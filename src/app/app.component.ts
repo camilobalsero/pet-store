aa896d66d7ca1131f178e0b25820655d336e1fa4
Camilo Balsero <camilobalsero16@gmail.com>
2024-04-02 15:01:44 -0500

    proyecto pet-store angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pet-store';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  
 }
